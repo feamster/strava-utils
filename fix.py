cf450516b8114569aed872b06da759d2f920300c
Nick Feamster <feamster@uchicago.edu>
2020-12-18 23:10:26 -0600

    code to get strava activities, selenium code to correct elevation, REST API code to rename activities

---
+++ fix.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+
+import time
+import sys
+import json
+from selenium import webdriver
+
+import re
+from datetime import datetime
+from geopy.geocoders import Nominatim
+import requests
+
+
+#from selenium.webdriver.firefox.options import Options as FirefoxOptions
+#options = FirefoxOptions()
+#options.add_argument("--headless")
+
+def get_tokens(tf):
+    with open(tf) as tokenFile:
+        strava_tokens = json.load(tokenFile)
+    return strava_tokens
+
+def get_activities(af):
+    with open(af) as tokenFile:
+        activities = json.load(tokenFile)
+    return activities
+
+def site_login(driver):
+    with open('conf/account.json') as accountFile:
+        account = json.load(accountFile)
+
+
+    driver.get("https://strava.com/login")
+    driver.find_element_by_id('email').send_keys(account['username'])
+    driver.find_element_by_id('password').send_keys(account['password'])
+    driver.find_element_by_id('login-button').click()
+
+def correct_elevation(driver,act_id):
+    activity_url = 'https://www.strava.com/activities/{}'.format(act_id)
+    driver.get(activity_url)
+    driver.find_element_by_xpath('//*[@id="elevation-adjusted-help"]').click()
+    driver.find_element_by_xpath('/html/body/div[6]/div[1]/a').click()
+    time.sleep(4)
+
+def elevation_check(activity,thresh):
+    elevation = activity['total_elevation_gain']
+    distance = activity['distance']
+    if distance == 0:
+        return -1
+    ratio = float(elevation) / float(distance) 
+    if ratio > thresh:
+        print(activity['id'], activity['date'], elevation, distance, ratio)
+        return activity['id']
+    else:
+        return -1
+
+def elevation_fix(driver,activities,thresh=0.01):
+    for activity in activities:
+        try:
+            if elevation_check(activity,thresh) > 0 and activity['workout_type'] == 0:
+                correct_elevation(driver,activity['id'])
+                continue
+        except:
+            continue
+
+def name_fix(activities,tokens):
+    app = Nominatim(user_agent="strava-fix")
+
+    for activity in activities:
+        name = activity['name']
+
+        # convert distance from meters to miles
+        distance = activity['distance']/1609.344
+
+        if re.match(r'.*?(Morning|Afternoon|Evening) Run.*', name):  #name == 'Morning Run':
+
+            if activity['start_latlng'] is None:
+                continue
+
+            dt = datetime.strptime(activity['start_date'], '%Y-%m-%dT%H:%M:%SZ')
+            print(name, dt)
+
+            (lat, lon) = activity['start_latlng']
+            coordinates = f"{lat}, {lon}"
+            location = app.reverse(coordinates, language='en').raw
+            #print(location)
+
+            try: 
+                aname = '{}/{}/{} {}, {} ({}) - {:4.2f} miles'.format( dt.month, dt.day, dt.year, 
+                        location['address']['city'], location['address']['state'], 
+                        location['address']['municipality'], distance)
+            except KeyError as e:
+                try:
+                    aname = '{}/{}/{} {}, {} - {:4.2f} miles'.format( dt.month, dt.day, dt.year, 
+                            location['address']['city'], location['address']['state'], distance)
+                except KeyError as e:
+                    aname = '{}/{}/{} Unknown - {:4.2f} miles'.format( dt.month, dt.day, dt.year, distance)
+
+            print(aname)
+            activity['name'] = aname
+
+            print("Updating {}".format(activity['id']))
+            #print(activity)
+            access_token = tokens['access_token']
+            headers = {'Authorization': 'Bearer ' + access_token, "Content-Type": "application/json"}
+            url = 'https://www.strava.com/api/v3/activities/{}'.format(activity['id'])
+            activity_json = json.dumps(activity)
+            response = requests.put(url, data=activity_json, headers=headers)
+            print(response.content)
+            time.sleep(1)
+
+
+##############
+# Activity Name Fix
+
+tokens = get_tokens('conf/strava_tokens.json')
+activities = get_activities('data/activities0.json')
+#activities = get_activities('data/activities-all-20201218.json')
+name_fix(activities,tokens)
+
+##############
+# Fix the Elevations - Chicago is Flat, so we look for a pretty low threshold
+
+driver = webdriver.Firefox()
+site_login(driver)
+elevation_fix(driver,activities,0.01)
---
+++ get_activities.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+import time
+
+# Step 1: url for when you need new code
+# https://www.strava.com/oauth/authorize?scope=read,activity:read_all,activity:write,profile:read_all,read_all&client_id=58450&response_type=code&redirect_uri=http://localhost:8888/strava-call-back.php&approval_prompt=force
+
+# Step 2: get access token
+# curl -X POST https://www.strava.com/oauth/token -F client_id=[client_id] -F client_secret=[client_secret] -F code=[CODE from URL] -F grant_type=authorization_code
+
+# client_id is specific to Strava username 
+newTokenUrl = "https://www.strava.com/oauth/authorize?scope=read,activity:read_all,profile:read_all,read_all&client_id=&response_type=code&redirect_uri=http://localhost:8888/strava-call-back.php&approval_prompt=force"
+
+###########################
+
+def get_activities(start=1, end=2):
+
+    # pages: 200 results per page, so by default we fetch 20 pages.
+    # there is a small bug, outputting "][" in between each JSON dump
+
+    f = open('data/activities0.json', 'w')
+    for p in range (start,end):
+        print(p)
+        #Get activities
+        url = "https://www.strava.com/api/v3/activities?access_token="
+        pstr = '&page={}&per_page=200'.format(p)
+
+        res = requests.get(url + strava_tokens['access_token'] + pstr)
+        data = res.json()
+
+        #Write activities json to my activities json file
+        #with open('data/activities4.json', 'w') as activitiesFile:
+        #    json.dump(data, activitiesFile)
+        json.dump(data,f)
+    f.close()
+
+###########################
+
+#Makes request for latest activity data with new token if expired
+#load token files
+with open('conf/strava_tokens.json') as tokenFile:
+    strava_tokens = json.load(tokenFile)
+with open('conf/account.json') as myInfo:
+    myInfo = json.load(myInfo)
+
+
+#If current token has expired make post request for new one
+if strava_tokens['expires_at'] < time.time():
+    res = requests.post(
+        url = 'https://www.strava.com/oauth/token',
+        data = {
+            'client_id': myInfo['client_id'],
+            'client_secret': myInfo['client_secret'],
+            'grant_type': 'refresh_token',
+            'refresh_token': strava_tokens['refresh_token'],
+            'access_token': strava_tokens['access_token']
+            }
+    )
+
+    newtokens = res.json()
+
+    #update token file
+    with open('conf/strava_tokens.json', 'w') as newStravaTokens:
+        json.dump(newtokens, newStravaTokens)
+        strava_tokens = newtokens
+
+
+get_activities()
